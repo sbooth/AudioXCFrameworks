a40ee418e0ccb298eee15f61506ec76330616ece
Stephen F. Booth <me@sbooth.org>
2020-10-29 18:23:48 -0500

    Add ogg

---
+++ ogg/download.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+LIBOGG_ARCHIVE=libogg-1.3.4.tar.xz
+LIBOGG_DOWNLOAD_URL=https://downloads.xiph.org/releases/ogg/$LIBOGG_ARCHIVE
+LIBOGG_DIR=$(basename "$LIBOGG_ARCHIVE" .tar.xz)
+
+if ! [ -f "./$LIBOGG_ARCHIVE" ]; then
+	if ! [ -x "$(command -v curl)" ]; then
+		echo "Error: $LIBOGG_ARCHIVE not found and curl not present"
+		echo "Please download manually from"
+		echo $LIBOGG_DOWNLOAD_URL
+		exit 1
+	fi
+
+	curl -O "$LIBOGG_DOWNLOAD_URL"
+fi
+
+/usr/bin/tar -xf "./$LIBOGG_ARCHIVE"
+/bin/ln -s "./$LIBOGG_DIR" "./libogg-src"
