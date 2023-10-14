b47669580e32d78f713c4965834385cfafa3b2e2
Stephen F. Booth <me@sbooth.org>
2023-10-13 23:16:51 -0500

    Update to libopus 1.4

--- opus/config.h
+++ opus/config.h
@@ -188,7 +188,7 @@
 #define PACKAGE_NAME "opus"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "opus 1.3.1"
+#define PACKAGE_STRING "opus 1.4"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "opus"
@@ -197,7 +197,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.3.1"
+#define PACKAGE_VERSION "1.4"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
